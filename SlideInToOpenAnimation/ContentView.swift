91fa38a4a57a19c2d79bbbe3984cab430686f508
Alexander Römer <alex@roemer.design>
2019-12-24 10:17:08 +0100

    Initial commit

--- SlideInToOpenAnimation/ContentView.swift
+++ SlideInToOpenAnimation/ContentView.swift
@@ -9,8 +9,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var moveUo = false
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Image("back")
+            Text("Slide up to open")
+                .foregroundColor(.white)
+                .fontWeight(.bold)
+                .opacity(moveUo ? 1 : 0)
+                .offset(y: moveUo ? 410 : 460)
+                .animation(Animation.easeOut(duration: 1.8).repeatCount(50, autoreverses: false).delay(2))
+        }
+        .edgesIgnoringSafeArea(.all)
+        .background(Color.black)
+        .onAppear() {
+            self.moveUo.toggle()
+        }
     }
 }
 
